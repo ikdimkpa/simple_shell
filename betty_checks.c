d64a938da1a00d14c696e1db562cfe61f2675b08
ikdimkpa <ikennadimkpa@gmail.com>
2023-02-20 13:42:12 -0800

    betty_checks.c

---
+++ betty_checks.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - checks for days in the week.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int num;
+
+	do {
+		printf("Enter a number: ");
+		scanf("%d", &num);
+
+		switch (num)
+		{
+			case 1:
+				printf("Monday\n");
+				break;
+			case 2:
+				printf("Tuesday\n");
+				break;
+			case 3:
+				printf("Wednesday\n");
+				break;
+			case 4:
+				printf("Thursday\n");
+				break;
+			case 5:
+				printf("Friday\n");
+				break;
+			case 6:
+				printf("Saturday\n");
+				break;
+			case 7:
+				printf("Sunday\n");
+				break;
+		default:
+			printf("Invalid Command\n");
+		}
+	} while (num <= 7);
+
+	return (0);
+}
